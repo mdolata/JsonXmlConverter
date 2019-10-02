12f57d5ec15753d1f394fc6c77fff98642c972ec
mdolata <mateuszdolata2@gmail.com>
2019-10-02 22:42:07 +0200

    mini task 1

--- src/main/java/com/mdolata/jsonxmlconverter/Main.java
+++ src/main/java/com/mdolata/jsonxmlconverter/Main.java
@@ -1,8 +1,19 @@
 package com.mdolata.jsonxmlconverter;
 
+import java.util.Scanner;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 public class Main {
     public static void main(String[] args ){
-        var test = "test";
-        System.out.println(test);
+        Scanner scanner = new Scanner(System.in);
+        String word = scanner.nextLine();
+
+        var collect = Stream.of(word.split(""))
+                .map(s -> s + s)
+                .collect(Collectors.joining());
+
+        System.out.println(collect);
+
     }
 }
